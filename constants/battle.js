7fda051f9a0683a37f52fbb55e12020ceb69c734
ssac <suenkating@gmail.com>
2020-04-02 15:19:22 +0800

    Add battle constants

---
+++ constants/battle.js
@@ -0,0 +1,46 @@
+module.exports = {
+	ACTION: {
+		MOVE: 1,
+		ATTACK: 2,
+		SKILL: 3
+	},
+
+	MAP: {
+		PLAIN: 1,
+		MOUNTAIN: 2
+	},
+
+	MAP_GRID: {
+		PLAIN: 1,
+		HILL: 2,
+		GRASS: 3,
+		ROCK: 4,
+		WASTELAND: 5,
+		ANTI_HORSE: 6,
+		SHOAL: 7,
+		SEA: 8,
+		MOUNTAIN: 9,
+	},
+
+	WEATHER: {
+		SUN: 1,
+		RAIN: 2,
+	},
+
+	REACTION: {
+		DODGE: 1,
+		COUNTER: 2,
+		COVER: 3,
+		PROTECT: 4,
+	},
+
+	EFFECT: {
+		DAMAGE: 1,
+		STATUS: 2,
+		HEAL: 3,
+		NATURE: 4,
+		ANGER: 5,
+		HIDE: 6,
+		TAUNT: 7
+	}
+}
